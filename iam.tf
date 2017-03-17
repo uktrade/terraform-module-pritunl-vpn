d62d1e0b29eb59fe39db6f1c0100ccae07bb1c81
Bowen Sun <bowensun@gmail.com>
2017-03-17 12:56:14 +0000

    * initial commit

---
+++ iam.tf
@@ -0,0 +1,94 @@
+resource "aws_iam_role" "node-role" {
+  name = "${var.aws_conf["domain"]}-vpn-role"
+  assume_role_policy = "${file("${path.module}/policies/default-role.json")}"
+}
+
+resource "aws_iam_user_policy" "user-default-policy" {
+  name = "${var.aws_conf["domain"]}-user-default-policy"
+  policy = "${file("${path.module}/policies/default-policy.json")}"
+  user = "${aws_iam_user.node-user.name}"
+}
+
+resource "aws_iam_role_policy" "node-default-policy" {
+  name = "${var.aws_conf["domain"]}-vpn-default-policy"
+  policy = "${file("${path.module}/policies/default-policy.json")}"
+  role = "${aws_iam_role.node-role.id}"
+}
+
+data "template_file" "node-ebs-policy" {
+  template = "${file("${path.module}/policies/ec2-ebs-policy.json")}"
+
+  vars {
+    region = "${var.vpc_conf["region"]}"
+    account = "${var.aws_conf["account_id"]}"
+    vpc = "${var.vpc_conf["id"]}"
+  }
+}
+
+resource "aws_iam_user_policy" "user-ebs-policy" {
+  name = "${var.aws_conf["domain"]}-user-ebs-policy"
+  policy = "${data.template_file.node-ebs-policy.rendered}"
+  user = "${aws_iam_user.node-user.name}"
+}
+
+resource "aws_iam_role_policy" "node-ebs-policy" {
+  name = "${var.aws_conf["domain"]}-vpn-ebs-policy"
+  policy = "${data.template_file.node-ebs-policy.rendered}"
+  role = "${aws_iam_role.node-role.id}"
+}
+
+resource "aws_iam_instance_profile" "node-profile" {
+  name = "${var.aws_conf["domain"]}-vpn-profile"
+  path = "/"
+  roles = ["${aws_iam_role.node-role.name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "template_file" "route53_policy" {
+  template = "${file("${path.module}/policies/route53-policy.json")}"
+
+  vars {
+    zone_id = "${var.vpc_conf["zone_id"]}"
+  }
+}
+
+resource "aws_iam_user_policy" "user-route53-policy" {
+  name = "${var.aws_conf["domain"]}-user-route53-policy"
+  policy = "${data.template_file.route53_policy.rendered}"
+  user = "${aws_iam_user.node-user.name}"
+}
+
+resource "aws_iam_role_policy" "route53" {
+  name = "${var.aws_conf["domain"]}-vpn-route53-policy"
+  policy = "${data.template_file.route53_policy.rendered}"
+  role = "${aws_iam_role.node-role.name}"
+}
+
+resource "aws_kms_key" "ebs" {
+  description = "${var.aws_conf["domain"]} VPN EBS Key"
+}
+
+data "template_file" "role-kms" {
+  template = "${file("${path.module}/policies/role-kms-policy.json")}"
+
+  vars {
+    aws_region = "${var.aws_conf["region"]}"
+    aws_account_id = "${var.aws_conf["account_id"]}"
+    ebs_kms_arn = "${aws_kms_key.ebs.arn}"
+  }
+}
+
+resource "aws_iam_user_policy" "user-kms-policy" {
+  name = "${var.aws_conf["domain"]}-user-kms-policy"
+  policy = "${data.template_file.role-kms.rendered}"
+  user = "${aws_iam_user.node-user.name}"
+}
+
+resource "aws_iam_role_policy" "role-kms-policy" {
+  name = "${var.aws_conf["domain"]}-vpn-kms-policy"
+  policy = "${data.template_file.role-kms.rendered}"
+  role = "${aws_iam_role.node-role.id}"
+}
---
+++ main.tf
@@ -0,0 +1,171 @@
+variable "aws_conf" {
+  type = "map"
+  default = {}
+}
+
+variable "vpc_conf" {
+  type = "map"
+  default = {}
+}
+
+resource "random_shuffle" "vpn_az" {
+  input = ["${split(",", var.vpc_conf["availability_zones"])}"]
+  result_count = 1
+  keepers = {
+    vpc_id = "${var.vpc_conf["id"]}"
+  }
+}
+
+data "aws_subnet" "vpn_az" {
+  vpc_id = "${var.vpc_conf["id"]}"
+  availability_zone = "${random_shuffle.vpn_az.result.0}"
+  tags {
+    Type = "public"
+  }
+}
+
+data "aws_ami" "vpn-ami" {
+  most_recent = true
+  name_regex = "ubuntu/images/ebs-ssd/ubuntu-xenial-16.04-amd64-server"
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_eip" "vpn" {
+  vpn = "${var.vpc_conf["id"]}"
+}
+
+resource "aws_ebs_volume" "vpn" {
+  availability_zone = "${random_shuffle.nfs_az.result.0}"
+  type = "gp2"
+  size = 10
+  encrypted = true
+  kms_key_id = "${aws_kms_key.ebs.arn}"
+
+  tags {
+    Name = "${var.aws_conf["domain"]}-vpn"
+    Stack = "${var.aws_conf["domain"]}"
+    clusterid = "${var.aws_conf["domain"]}"
+    host-type = "vpn"
+    svc = "vpn"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = ["*"]
+  }
+}
+
+data "template_file" "vpn-cloudinit" {
+  template = "${file("${path.module}/cloudinit.yml")}"
+
+  vars {
+    aws_region = "${var.vpc_conf["region"]}"
+    dns_zone_id = "${var.vpc_conf["zone_id"]}"
+    cluster_id = "${var.aws_conf["domain"]}"
+    eip = "${aws_eip.vpn.id}"
+  }
+}
+
+resource "aws_launch_configuration" "vpn" {
+  name_prefix = "${var.aws_conf["domain"]}-vpn-"
+  image_id = "${data.aws_ami.default.id}"
+  instance_type = "${var.aws_conf["instance_type"]}"
+  key_name = "${var.aws_conf["key_name"]}"
+  iam_instance_profile = "${aws_iam_instance_profile.node-profile.id}"
+  security_groups = [
+    "${var.vpc_conf["security_group"]}",
+    "${aws_security_group.vpn.id}"
+  ]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+    delete_on_termination = false
+  }
+  user_data = "${data.template_file.vpn-cloudinit.rendered}"
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "vpn" {
+  name = "${var.aws_conf["domain"]}-vpn"
+  launch_configuration = "${aws_launch_configuration.vpn.name}"
+  vpc_zone_identifier = ["${split(",", var.vpc_conf["subnets_public"])}"]
+  min_size = 1
+  max_size = 1
+  desired_capacity = 1
+  wait_for_capacity_timeout = 0
+
+  tag {
+    key = "Name"
+    value = "${var.aws_conf["domain"]}-vpn"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "Stack"
+    value = "${var.aws_conf["domain"]}"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "clusterid"
+    value = "${var.aws_conf["domain"]}"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "host-type"
+    value = "vpn"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "svc"
+    value = "vpn"
+    propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "vpn" {
+  name = "${var.aws_conf["domain"]}-vpn"
+  vpc_id = "${var.vpc_conf["id"]}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.aws_conf["domain"]}-vpn"
+    Stack = "${var.aws_conf["domain"]}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "vpn" {
+  zone_id = "${var.vpc_conf["zone_id"]}"
+  name = "vpn.${var.aws_conf["domain"]}"
+  type = "A"
+  records = ["${aws_eip.vpn.public_ip}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
